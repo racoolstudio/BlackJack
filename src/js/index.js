9903b7635858ed4ee624b53066d1605bc59bc636
Ridwan Abdulwaheed <75172422+racoolstudio@users.noreply.github.com>
2023-11-19 02:35:55 -0330

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,41 @@
 
 let stat = document.getElementById("status");
-let button = document.getElementById("pick")
+let newCardButton = document.getElementById("pick")
+let startButton = document.getElementById("startGame")
+
 let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let sum = document.getElementById("sumNum");
-
+let isOn = true;
 function checkCondition() {
     if (Number(sum.innerText) < 21) {
-        stat.innerText = "Do You Want To Draw Another Card ?";
-        stat.style.color = "black";
+        stat.innerText = "Draw Another Card";
+        stat.style.color = "white";
+        isOn = true;
+        
     }
     else if (Number(sum.innerText) === 21) {
-        stat.textContent = "Whoo, You won !!! 🥳\nYou Can Pick Another";
+        stat.textContent = "Whoo, You won !!! 🥳";
         stat.style.color = "green";
+        isOn =false;
     }
     else {
         stat.textContent= "You are Out !!! 😭\nYou Can't Pick Cards AnyMore";
         stat.style.color = "red";
-        button.disabled = true;
+        isOn = false;
+    }
+
+    if (isOn){
+        startButton.disabled = true;
+        newCardButton.disabled = false;
+    }
+        else{
+        startButton.disabled = false;
+        newCardButton.disabled = true;
     }
 }
 
-function selectRandom(){
+function startGame(){
     num1R = Math.floor((Math.random() * 12) + 2);
     num2R = Math.floor((Math.random() * 12) + 2);
     sumR = num1R + num2R;
@@ -30,5 +44,14 @@ function selectRandom(){
     sum.innerText = sumR; 
     checkCondition();
 }
+
+function newCard(){
+    let newCard = Math.floor((Math.random() * 12) + 2);
+    num1.innerText = sum.innerText;
+    num2.innerText = newCard;
+    sum.innerText = Number(num1.innerText) + Number(num2.innerText);
+    checkCondition();
+}
+
 
 
